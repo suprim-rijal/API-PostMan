3c29692f05b10b1b9d290c98c08e72754a3bf881
suprim-rijal <suprimrijal1@gmail.com>
2026-09-05 14:26:14 +0300

    refactor(components): implement PageLinks in Navbar and create SocialLinks component

--- WebCourses/Week3/week3-fe-pair-programming/src/components/Footer.jsx
+++ WebCourses/Week3/week3-fe-pair-programming/src/components/Footer.jsx
@@ -1,42 +1,14 @@
 import PageLinks from "./PageLinks";
+import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
   return (
     <footer className="section footer">
       <PageLinks parentClass="footer-links" itemClass="footer-link" />
 
-      <ul className="footer-icons">
-        <li>
-          <a
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.x.com"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-x-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.squarespace.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-squarespace"></i>
-          </a>
-        </li>
-      </ul>
+      {/* Replaced hardcoded social icons with dynamic component */}
+      <SocialLinks parentClass="footer-icons" itemClass="footer-icon" />
+
       <p className="copyright">
         copyright &copy; Backroads travel tours company
         <span id="date">{new Date().getFullYear()}</span> all rights reserved
--- WebCourses/Week3/week3-fe-pair-programming/src/components/SocialLink.jsx
+++ WebCourses/Week3/week3-fe-pair-programming/src/components/SocialLink.jsx
@@ -1,6 +1,11 @@
-function SocialLink() {
-  return <div>SocialLink</div>;
-}
+const SocialLink = ({ href, icon, itemClass }) => {
+  return (
+    <li>
+      <a href={href} target="_blank" rel="noreferrer" className={itemClass}>
+        <i className={icon}></i>
+      </a>
+    </li>
+  );
+};
 
 export default SocialLink;
- 
