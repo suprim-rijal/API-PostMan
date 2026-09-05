20c8c8621cc5255d98f65ae21ddedd4a52b698cd
suprim-rijal <suprimrijal1@gmail.com>
2026-09-05 13:48:34 +0300

    All componetns to JSX

--- WebCourses/Week3/week3-fe-pair-programming/src/components/About.jsx
+++ WebCourses/Week3/week3-fe-pair-programming/src/components/About.jsx
@@ -1,10 +1,37 @@
+import aboutImg from "../assets/images/about.jpeg";
+
 const About = () => {
   return (
-    <div>
-      <p className="section-dummy">About</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <section className="section" id="about">
+      <div className="section-title">
+        <h2>
+          about <span>us</span>
+        </h2>
+      </div>
+
+      <div className="section-center about-center">
+        <div className="about-img">
+          <img src={aboutImg} className="about-photo" alt="awesome beach" />
+        </div>
+        <article className="about-info">
+          <h3>explore the difference</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <a href="#about" className="btn">
+            read more
+          </a>
+        </article>
+      </div>
+    </section>
   );
 };
+
 export default About;
- 
--- WebCourses/Week3/week3-fe-pair-programming/src/components/Footer.jsx
+++ WebCourses/Week3/week3-fe-pair-programming/src/components/Footer.jsx
@@ -1,11 +1,66 @@
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      <p className="section-dummy">Footer</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <footer className="section footer">
+      <ul className="footer-links">
+        <li>
+          <a href="#home" className="footer-link">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="footer-link">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="#services" className="footer-link">
+            services
+          </a>
+        </li>
+        <li>
+          <a href="#tours" className="footer-link">
+            featured
+          </a>
+        </li>
+      </ul>
+      <ul className="footer-icons">
+        <li>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-icon"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.x.com"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-icon"
+          >
+            <i className="fab fa-x-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.squarespace.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-icon"
+          >
+            <i className="fab fa-squarespace"></i>
+          </a>
+        </li>
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
   );
-}
+};
 
 export default Footer;
- 
