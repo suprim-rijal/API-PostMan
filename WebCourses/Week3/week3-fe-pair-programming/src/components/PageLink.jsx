fc5351b1c3b05429abc449076235be53f48329fd
suprim-rijal <suprimrijal1@gmail.com>
2026-09-05 14:21:12 +0300

    refactor(components): extract PageLink and PageLinks, update Footer

--- WebCourses/Week3/week3-fe-pair-programming/src/components/PageLink.jsx
+++ WebCourses/Week3/week3-fe-pair-programming/src/components/PageLink.jsx
@@ -1,6 +1,11 @@
-function PageLink() {
-  return <div>PageLink</div>;
-}
+const PageLink = ({ link, itemClass }) => {
+  return (
+    <li key={link.id}>
+      <a href={link.href} className={itemClass}>
+        {link.text}
+      </a>
+    </li>
+  );
+};
 
 export default PageLink;
- 
